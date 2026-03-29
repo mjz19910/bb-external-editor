b038efa13d8e7089bf671e94a4a3751878fb7b6c
mjz19910 <matthias291999@gmail.com>
2026-03-29 05:51:59 -0700

    .

--- servers/home/init_darknet.ts
+++ servers/home/init_darknet.ts
@@ -1,17 +1,19 @@
 import { Darknet, WithPort } from "./darknet_paths";
 
+const dnet_files_dyn: string[] = [];
+dnet_files_dyn.push("darknet_probe.ts");
+dnet_files_dyn.push("darknet_paths.ts");
+dnet_files_dyn.push("darknet/misc.ts");
+dnet_files_dyn.push("NetscriptDefinitions.d.ts");
+dnet_files_dyn.push(Darknet.MemoryReallocation);
+dnet_files_dyn.push(WithPort.Read);
+dnet_files_dyn.push("type/helper.ts");
+
 export function main(ns: NS) {
 	const f = ns.flags([["threads", 2], ["port", 1]]) as {
 		threads: number;
 		port: number;
 	};
-	const dnet_files_dyn: string[] = [];
-	dnet_files_dyn.push("darknet_probe.ts");
-	dnet_files_dyn.push("darknet_paths.ts");
-	dnet_files_dyn.push("darknet/misc.ts");
-	dnet_files_dyn.push("NetscriptDefinitions.d.ts");
-	dnet_files_dyn.push(Darknet.MemoryReallocation);
-	dnet_files_dyn.push(WithPort.Read);
 	const local_probe = ns.dnet.probe();
 	if (local_probe.length == 1 && local_probe[0] == "darkweb") {
 		ns.scp(dnet_files_dyn, "darkweb", "home");
