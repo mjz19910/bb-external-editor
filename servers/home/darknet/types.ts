7f0215bebb1c1c8f2731c4707e524c09146ccb27
mjz19910 <matthias291999@gmail.com>
2026-03-29 05:57:15 -0700

    .

--- servers/home/darknet/types.ts
+++ servers/home/darknet/types.ts
@@ -1,21 +1,26 @@
 export type ServerAuthDetails2 = {
-	isOnline: boolean
-	isConnectedToCurrentServer: boolean
-	hasSession: boolean
-	modelId: string
-	passwordHint: string
-	data: string
-	logTrafficInterval: number
-	passwordLength: number
-	passwordFormat: "numeric" | "alphabetic" | "alphanumeric" | "ASCII" | "unicode"
-}
+	isOnline: boolean;
+	isConnectedToCurrentServer: boolean;
+	hasSession: boolean;
+	modelId: string;
+	passwordHint: string;
+	data: string;
+	logTrafficInterval: number;
+	passwordLength: number;
+	passwordFormat:
+		| "numeric"
+		| "alphabetic"
+		| "alphanumeric"
+		| "ASCII"
+		| "unicode";
+};
 export type DarknetServerInfo = {
-	connectedToParent: boolean
-	authDetails: ServerAuthDetails2 | null
-	server: { hostname: string } | null
-	parent: string | null
-	password: string | null
-}
+	connectedToParent: boolean;
+	authDetails: ServerAuthDetails2 | null;
+	server: { hostname: string } | null;
+	parent: string | null;
+	password: string | null;
+};
 export const darknet_files = [
 	"api/darknet/probe.ts",
 	"api/darknet/stasis.ts",
@@ -24,4 +29,4 @@ export const darknet_files = [
 	"api/port_file_read.ts",
 	"api/darknet/open_cache.ts",
 	"api/darknet/update_probe.ts",
-]
+];
