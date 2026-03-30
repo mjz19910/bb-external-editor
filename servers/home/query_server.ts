72741b472c39d9ef2db3dd65555e8a1df377c615
mjz19910 <matthias291999@gmail.com>
2026-03-30 02:24:39 -0700

    .

--- servers/home/query_server.ts
+++ servers/home/query_server.ts
@@ -7,8 +7,8 @@ import { write_info_to_fs_db } from "./write_ip_db";
 export async function main(ns: NS) {
 	const port = new ScriptPort(ns, 1);
 	const port2 = new ScriptPort(ns, 2);
-	for await (const item of start_read_loop<string>(port2)) {
-		const [query_cmd, query_arg] = item.split(" ");
+	for await (const { v } of start_read_loop<string>(port2)) {
+		const [query_cmd, query_arg] = v.split(" ");
 		const args = query_arg === "" ? [] : query_arg.split(",");
 		handle_query(ns, port, query_cmd, args);
 	}
@@ -51,16 +51,16 @@ function handle_online_check(
 	});
 }
 
-async function* start_read_loop<T>(
-	port: ScriptPort<unknown>,
-): AsyncIterableIterator<T> {
+type A<T> = AsyncGenerator<{ v: T }, void, void>;
+type B<T> = ScriptPort<T>;
+async function* start_read_loop<T>(port: B<T>): A<T> {
 	for (;;) {
 		const res = port.readOpt<T>();
 		if (res.type === "None") {
 			await port.nextWrite();
 			continue;
 		}
-		yield res.value;
+		yield { v: res.value };
 	}
 }
 
