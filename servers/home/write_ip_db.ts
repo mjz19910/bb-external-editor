07b02e23600855661831216da4b2cca507b6137b
mjz19910 <matthias291999@gmail.com>
2026-03-30 01:14:20 -0700

    .

--- servers/home/write_ip_db.ts
+++ servers/home/write_ip_db.ts
@@ -5,18 +5,8 @@ export function write_info_to_fs_db(
 	info: DarknetServerInfo,
 	cur_data?: string,
 ) {
-	const srv = info.server;
-	if (srv.ip === void 0 || srv.ip === "") {
-		ns.tprint("missing ip field on srv.ip ", info);
-		return;
-	}
-	const host = info.server.hostname;
-	if (host === void 0 || host === "") {
-		ns.tprint("missing hostname for ", info.server.ip);
-		return;
-	}
-	const ip_save_path = `tmp/ip/${srv.ip}.txt`;
-	const host_save_path = `tmp/host/${host.replaceAll(/[:]/g, "_")}.txt`;
+	const ip_save_path = `tmp/ip/${info.ip}.txt`;
+	const host_save_path = `tmp/host/${info.host.replaceAll(/[:]/g, "_")}.txt`;
 	const new_content = JSON.stringify(info, void 0, "\t");
 	if (new_content !== cur_data) {
 		ns.write(ip_save_path, new_content, "w");
