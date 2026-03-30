559abcfed83491fcba631f749d2b4f38b378baee
mjz19910 <matthias291999@gmail.com>
2026-03-29 21:16:28 -0700

    .

--- servers/darkweb/darknet/probe_msg.ts
+++ servers/darkweb/darknet/probe_msg.ts
@@ -6,13 +6,23 @@ type SendMessage = {
 	reply_port: 4;
 };
 
+type ReplyMessage = {
+	type: "darknet.probe";
+	for: string;
+	alt: "ip";
+	results: string[];
+};
+
+type ProbeMessageTypes = SendMessage | ReplyMessage;
+
 export async function main(ns: NS) {
 	const args = ns.args;
 	if (typeof args[0] != "string") {
 		ns.tprint("missing current host as arg");
 		return;
 	}
-	const port = new ScriptPort(ns, 1);
+	ns.ui.openTail();
+	const port = new ScriptPort<ProbeMessageTypes>(ns, 1);
 	const port4 = new ScriptPort(ns, 4);
 	const ips = ns.dnet.probe(true);
 	port.write<SendMessage>({
@@ -22,9 +32,9 @@ export async function main(ns: NS) {
 	});
 	await port4.nextWrite();
 	ns.print(port4.read<unknown>());
-	port.write({
+	port.write<ReplyMessage>({
 		type: "darknet.probe",
-		for: ns.args[0],
+		for: args[0],
 		alt: "ip",
 		results: ips,
 	});
