36c155036bf1bb0a922d5d71db1ec5d099a3d368
mjz19910 <matthias291999@gmail.com>
2026-03-29 23:30:05 -0700

    .

--- servers/darkweb/rm_all_js_files.ts
+++
@@ -1,6 +0,0 @@
-export async function main(ns: NS) {
-	const files = ns.ls(ns.getHostname(), ".js");
-	for (const file of files) {
-		ns.rm(file);
-	}
-}
--- servers/darkweb/scp.ts
+++
@@ -1,4 +0,0 @@
-export async function main(ns: NS) {
-	const args = ns.args as [string, string, string];
-	ns.scp(...args);
-}
