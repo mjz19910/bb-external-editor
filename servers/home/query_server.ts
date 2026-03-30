392e88d1762ee65e636e9b2efd4bfa8eea44082e
mjz19910 <matthias291999@gmail.com>
2026-03-30 00:59:39 -0700

    .

--- servers/home/query_server.ts
+++ servers/home/query_server.ts
@@ -1,6 +1,7 @@
 import { DarknetServer } from "./darknet/misc";
 import { DarknetServerInfo } from "./darknet/types";
 import { ScriptPort } from "./type/ScriptPort";
+import { write_info_to_fs_db } from "./write_ip_db";
 
 export const utf8_bad_chars = {
 	":": String.fromCharCode(61440 + ":".charCodeAt(0)),
@@ -68,26 +69,13 @@ export async function main(ns: NS) {
 			continue;
 		}
 		const srv = ns.getServer(info.server.ip) as DarknetServer;
-		let nt_ok_path = host.replaceAll(/[-:;^&@%$]/g, "_");
-		nt_ok_path = nt_ok_path.replaceAll(/__+/g, "_");
-		nt_ok_path = nt_ok_path.replaceAll("🅱️", "b");
-		const host_save_path = `tmp/host/${nt_ok_path}.txt`;
 		if (!srv.isOnline) {
 			ns.tprint("server offline ", host, "(", oSrv.ip, ")");
-			ns.rm(file);
 			oSrv.isOnline = false;
-			const new_content = JSON.stringify(info, void 0, "\t");
-			ns.write(host_save_path, new_content, "w");
+			write_info_to_fs_db(ns, info);
 			continue;
 		}
 		info.server = srv;
-		const new_content = JSON.stringify(info, void 0, "\t");
-		if (new_content != file_data) {
-			ns.write(file, new_content, "w");
-			ns.write(host_save_path, new_content, "w");
-		}
-		if (!ns.fileExists(host_save_path)) {
-			ns.write(host_save_path, new_content, "w");
-		}
+		write_info_to_fs_db(ns, info);
 	}
 }
--- servers/home/write_ip_db.ts
+++ servers/home/write_ip_db.ts
@@ -1,18 +1,24 @@
 import { DarknetServerInfo } from "./darknet/types";
 
 export function write_info_to_fs_db(ns: NS, info: DarknetServerInfo) {
-	if (info.server.ip === void 0) {
+	const srv = info.server;
+	if (srv.ip === void 0 || srv.ip === "") {
 		ns.tprint("missing ip field on srv.ip ", info);
 		return;
 	}
-	ns.write(
-		`tmp/ip/${info.server.ip}.txt`,
-		JSON.stringify(info, void 0, "\t"),
-		"w",
-	);
-	ns.write(
-		`tmp/ip/${info.server.ip}.txt`,
-		JSON.stringify(info, void 0, "\t"),
-		"w",
-	);
+	const new_content = JSON.stringify(info, void 0, "\t");
+	if (srv.isOnline) {
+		const ip_save_path = `tmp/ip/${srv.ip}.txt`;
+		ns.write(ip_save_path, new_content, "w");
+	}
+	const host = info.server.hostname;
+	if (host === void 0 || host === "") {
+		ns.tprint("missing hostname for ", info.server.ip);
+		return;
+	}
+	let nt_ok_path = host.replaceAll(/[-:;^&@%$]/g, "_");
+	nt_ok_path = nt_ok_path.replaceAll(/__+/g, "_");
+	nt_ok_path = nt_ok_path.replaceAll("🅱️", "b");
+	const host_save_path = `tmp/host/${nt_ok_path}.txt`;
+	ns.write(host_save_path, new_content, "w");
 }
